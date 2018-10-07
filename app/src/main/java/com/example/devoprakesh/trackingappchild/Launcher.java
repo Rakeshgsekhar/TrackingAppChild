cdbc5e688df090d6feb9c9e3bca92a4db1d09b2d
Rakeshgsekhar <rakeshgsekhar.fortune@gmail.com>
2018-10-07 11:31:43 +0530

    Trackre Service and login Started

--- app/src/main/java/com/example/devoprakesh/trackingappchild/Launcher.java
+++ app/src/main/java/com/example/devoprakesh/trackingappchild/Launcher.java
@@ -1,14 +1,32 @@
 package com.example.devoprakesh.trackingappchild;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Launcher extends AppCompatActivity {
 
+    private SharedPreferences sharedPreferences;
+    private String uid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+
+        sharedPreferences = getSharedPreferences("ChildLoginDetails",MODE_PRIVATE);
+
+        uid = sharedPreferences.getString("UID",null);
+
+
+        if(uid==null){
+
+            Intent intent = new Intent(Launcher.this,Register.class);
+            startActivity(intent);
+        }else{
+            Intent intent = new Intent(Launcher.this,Home.class);
+            startActivity(intent);
+        }
 
 
     }
