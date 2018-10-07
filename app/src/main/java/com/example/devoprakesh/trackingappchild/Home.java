2b02a5f884d6dddb157ab902e9b7949f8e95c040
Rakeshgsekhar <rakeshgsekhar.fortune@gmail.com>
2018-10-07 11:44:59 +0530

    tracker app

--- app/src/main/java/com/example/devoprakesh/trackingappchild/Home.java
+++ app/src/main/java/com/example/devoprakesh/trackingappchild/Home.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 
 public class Home extends AppCompatActivity {
 
+    private static final int PERMISSION_REQUEST = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
     }
 }
--- app/src/main/java/com/example/devoprakesh/trackingappchild/TrackerService.java
+++ app/src/main/java/com/example/devoprakesh/trackingappchild/TrackerService.java
@@ -11,6 +11,28 @@ public class TrackerService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        buildNotification();
+        loginToFirebase();
+    }
+
+
+    public void buildNotification(){
+
+
+    }
+
+
+
+
+    public void loginToFirebase(){
+
+
     }
 }
